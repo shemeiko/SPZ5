ac509975372da1347be419976d69f1a4b224ce72
Shemeiko Taras <shemiks2005@gmail.com>
2025-11-17 00:20:30 +0200

    init

--- CoR.cpp
+++ CoR.cpp
@@ -2,18 +2,28 @@
 #include <memory>
 
 struct Request {
+    enum class Type {
+        Email,
+        Password,
+        Age
+    };
+    explicit Request(Type t) : type(t) {}
     virtual ~Request() = default;
+    Type type;
 };
 
 struct Email : Request {
+    Email() : Request(Type::Email) {};
     std::string email;
 };
 
 struct Password : Request {
+    Password() : Request(Type::Password) {};
     std::string password;
 };
 
 struct Age : Request {
+    Age() : Request(Type::Age) {};
     int age;
 };
 
@@ -39,11 +49,11 @@ protected:
 
 class EmailHandler : public ValidationHandler {
     bool process(const Request& request) override {
-        const Email* emailReq = dynamic_cast<const Email*>(&request);
-        if (!emailReq) {
+        const Email& emailReq = static_cast<const Email&>(request);
+        if (emailReq.type != Request::Type::Email) {
             std::cout << "  EmailValidator: passed requests doesn't have \'email\' field" << '\n';
         } else {
-            if (emailReq->email.find('@') != std::string::npos)
+            if (emailReq.email.find('@') != std::string::npos)
             std::cout << "  EmailValidator: email is valid" << '\n';
             else {
                 std::cout << "  EmailValidator: email is invalid. Should contain \'at\' symbol (@)" << '\n';
@@ -57,11 +67,11 @@ class EmailHandler : public ValidationHandler {
 
 class PasswordHandler : public ValidationHandler {
     bool process(const Request& request) override {
-        const Password* passReq = dynamic_cast<const Password*>(&request);
-        if (!passReq) {
+        const Password& passReq = static_cast<const Password&>(request);
+        if (passReq.type != Request::Type::Password) {
             std::cout << "  PasswordValidator: passed requests doesn't have \'password\' field" << '\n';
         } else {
-            if (passReq->password.length() >= 6)
+            if (passReq.password.length() >= 6)
                 std::cout << "  PasswordValidator: Password is valid" << '\n';
             else {
                 std::cout << "  PasswordValidator: is invalid. Length should be greater than 6" << '\n';
@@ -75,11 +85,11 @@ class PasswordHandler : public ValidationHandler {
 
 class AgeHandler : public ValidationHandler {
     bool process(const Request& request) override {
-        const Age* ageReq = dynamic_cast<const Age*>(&request);
-        if (!ageReq) {
+        const Age& ageReq = static_cast<const Age&>(request);
+        if (ageReq.type != Request::Type::Age) {
             std::cout << "  AgeValidator: passed requests doesn't have \'age\' field" << '\n';
         } else {
-            if (ageReq->age > 0 && ageReq->age < 120)
+            if (ageReq.age > 0 && ageReq.age < 120)
                 std::cout << "  AgeValidator: age is valid" << '\n';
             else {
                 std::cout << "  AgeValidator: Invalid age. Ranges [1; 119]" << '\n';
@@ -102,7 +112,7 @@ int main() {
     Email emailReq{};
     emailReq.email = "example@example.com";
     Password passReq{};
-    passReq.password = "12345";
+    passReq.password = "123456";
     Age ageReq{};
     ageReq.age = 20;
 
